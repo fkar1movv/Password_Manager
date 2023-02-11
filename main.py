431e88fc6053c60a42253438db24f62a0c16595d
fkar1movv <fazliddinkarimov1701@gmail.com>
2023-02-11 19:52:47 +0500

    Generate password function and Save functions added. And little changes in design.

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from tkinter import *
+import random
 
+# ---------------------------   SAVE PASSWORD --------------------------------
+def save():
+    with open("data.txt", "a") as file:
+        file.write(website_input.get()+" | "+email_input.get()+" | "+password_input.get()+"\n")
+    website_input.delete(0, END)
+    email_input.delete(0, END)
+    password_input.delete(0, END)
+
+
+# ---------------------------   PASSWORD GENERATOR  --------------------------
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+index = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+nr_letters = random.choice(index)
+nr_symbols = random.choice(index)
+nr_numbers = random.choice(index)
+
+letter = ''
+symbol = ''
+number = ''
+for x in range(nr_letters):
+  letter += random.choice(letters)
+for x in range(nr_symbols):
+  symbol += random.choice(symbols)
+for x in range(nr_numbers):
+  number += random.choice(numbers)
+result = letter + symbol + number
+arr = []
+for x in range(len(result)):
+    arr.append(result[x])
+random.shuffle(arr)
+shuffled = ''
+for x in arr:
+    shuffled += x
+def generate_password():
+    password_input.insert(0, shuffled)
+
+# ---------------------------   UI SETUP  ------------------------------------
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=25, bg="white")
@@ -13,6 +54,7 @@ website = Label(text="Website:", bg="white")
 website.grid(row=1, column=0)
 website_input = Entry(width=67)
 website_input.grid(row=1, column=1, columnspan=2)
+website_input.focus()
 
 email = Label(text="Email/Username:", bg="white")
 email.grid(row=2, column=0)
@@ -23,16 +65,10 @@ password = Label(text="Password:", bg="white")
 password.grid(row=3, column=0)
 password_input = Entry(width=47)
 password_input.grid(row=3, column=1)
-generate_pwd = Button(text="Generate Password")
+generate_pwd = Button(text="Generate Password", command=generate_password)
 generate_pwd.grid(row=3, column=2)
 
-add = Button(text="Add", width=57)
+add = Button(text="Add", width=57, command=save)
 add.grid(row=4, column=1, columnspan=2)
-
-
-
-
-
-
 
 window.mainloop()
