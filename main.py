f2c1bbdf2e9264aa7a30ae39c1e9fd8f112733c6
fkar1movv <fazliddinkarimov1701@gmail.com>
2023-05-08 00:29:22 +0500

    Search function added and saving information in JSON file

--- main.py
+++ main.py
@@ -2,23 +2,38 @@ from tkinter import *
 from tkinter import messagebox as mbox
 import random
 import pyperclip
+import json
 
 # ---------------------------   SAVE PASSWORD --------------------------------
 def save():
     website_data = website_input.get()
     email_data = email_input.get()
     password_data = password_input.get()
-
+    new_data = {
+        website_data: {
+            "email": email_data,
+            "password": password_data
+        }
+    }
     if website_data == "" or email_data == "" or password_data == "":
         mbox.showerror(title="Error", message="Do not left empty!!!")
     else:
         is_ok = mbox.askokcancel(title="Check", message="Is it ok? Or do you want to change it?")
         if is_ok:
-            with open("data.txt", "a") as file:
-                file.write(website_data + " | " + email_data + " | " + password_data + "\n")
-            website_input.delete(0, END)
-            email_input.delete(0, END)
-            password_input.delete(0, END)
+            try:
+                with open("data.json", "r") as file:
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open("data.json", "w") as file:
+                    json.dump(new_data, file)
+            else:
+                data.update(new_data)
+                with open("data.json", "w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                website_input.delete(0, END)
+                email_input.delete(0, END)
+                password_input.delete(0, END)
 
 
 
@@ -53,6 +68,21 @@ def generate_password():
     pyperclip.copy(shuffled)
     password_input.insert(0, shuffled)
 
+# ---------------------------   SEARCH FUNCTION  -----------------------------
+
+def search_website():
+    website = website_input.get()
+    try:
+        with open("data.json", "r") as data_file:
+            fetched_data = json.load(data_file)
+    except FileNotFoundError:
+        mbox.showerror(title="No Data", message="No data file found!!!")
+    else:
+        if website in fetched_data:
+            mbox.showinfo(title=website, message=f"Email: ${fetched_data[website]['email']} \nPassword: ${fetched_data[website]['password']}")
+        else:
+            mbox.showerror(title=website, message="Website not found")
+
 # ---------------------------   UI SETUP  ------------------------------------
 window = Tk()
 window.title("Password Manager")
@@ -65,8 +95,8 @@ canvas.grid(row=0, column=1)
 
 website = Label(text="Website:", bg="white")
 website.grid(row=1, column=0)
-website_input = Entry(width=67)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input = Entry(width=47)
+website_input.grid(row=1, column=1)
 website_input.focus()
 
 email = Label(text="Email/Username:", bg="white")
@@ -83,5 +113,8 @@ generate_pwd.grid(row=3, column=2)
 
 add = Button(text="Add", width=57, command=save)
 add.grid(row=4, column=1, columnspan=2)
+
+search = Button(text="Search", width=15, command=search_website)
+search.grid(row=1, column=2)
 
 window.mainloop()
